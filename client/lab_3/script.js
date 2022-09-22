328f52cd09a98bf0f53e4f8685c05f65fec6353a
Yabing Yang <98364345+yyang7171@users.noreply.github.com>
2022-09-21 23:41:08 -0400

    updates prevslide func js

--- client/lab_3/script.js
+++ client/lab_3/script.js
@@ -37,7 +37,7 @@ function moveToNextSlide() {
 }
 function moveToPrevSlide() {
   if (slidePosition === 0) {
-    slidePosition = totalSlides;
+    slidePosition = totalSlides - 1;
   } else {
     slidePosition -= 1;
   }
