c93dc2105b8091c5796530b3e60ad7a826f90052
Yabing Yang <98364345+yyang7171@users.noreply.github.com>
2022-09-23 11:32:22 -0400

    syntax edit minor

--- client/Tetris/script.js
+++ client/Tetris/script.js
@@ -178,11 +178,11 @@ document.addEventListener('DOMContentLoaded', () => {
   // add score
 
   function addScore() {
-    for (let i = 0; i < 199; i +=width) {
-      const row = [i, i+1, i+2, i+3, i+4, i+5, i+6, i+7, i+8, i+9];
-      if(row.every((index) => squares[index].classList.contains('taken'))) {
-        score +=10;
-        scoreDisplay.innerHTML = score;
+    for (let i = 0; i < 199; i += width) {
+      const row = [i, i + 1, i + 2, i + 3, i + 4, i + 5, i + 6, i + 7, i + 8, i + 9];
+      if (row.every((index) => squares[index].classList.contains('taken'))) {
+        score += 10;
+        ScoreDisplay.innerHTML = score;
         row.forEach((index) => {
           squares[index].classList.remove('taken');
         });
