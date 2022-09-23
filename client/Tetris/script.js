40353d0ba9aa623225a9455ac825b602d4475ff9
Yabing Yang <98364345+yyang7171@users.noreply.github.com>
2022-09-23 11:37:23 -0400

    updates addScore

--- client/Tetris/script.js
+++ client/Tetris/script.js
@@ -185,6 +185,7 @@ document.addEventListener('DOMContentLoaded', () => {
         ScoreDisplay.innerHTML = score;
         row.forEach((index) => {
           squares[index].classList.remove('taken');
+          squares[index].classList.remove('tetromino');
         });
         const squaresRemoved = squares.splice(i, width);
         squares = squaresRemoved.concat(squares);
